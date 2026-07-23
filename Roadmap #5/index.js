a6dbff280a1586d0055dd9d0c97b95e30834c8ca
brmenn <bayuw8909@gmail.com>
2026-07-23 14:07:13 +0700

    Create index.js

---
+++ Roadmap #5/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const app = require('./src/app');
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
